d731bf7fc30ba0ec60688e2d9269a9b83cbcfe0b
ADITYASUROSE2003 <159411911+ADITYASUROSE2003@users.noreply.github.com>
2024-05-02 18:39:07 +0530

    Add files via upload

---
+++ Addition.java
@@ -0,0 +1,9 @@
+public class Addition{
+    public static void main(string[]arg){
+        int a= 4;
+        int b= 3;
+       int sum =a+b;
+        systum.out.println(a+" "+b+" "+sum);
+    }
+
+}
